0bb7d76ca362ec9bc462b7ba056378eef1a3b4dc
Or Bachar <or@deweb.io>
2024-07-18 16:13:13 +0300

    update deployment scripts

--- scripts/deployment/config.js
+++ scripts/deployment/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    "network": "rinkeby",
+    "network": "sepolia",
     "mint": {
         "totalSupply": "1000000000"
     },
     "safe" : {
         "address" : "0x3B3944D7351c69944DCFEc85651394dC38326a4d"
     },
-    "rewards": [ { "q" : 0, "amount": "200000"}, { "q" : 1, "amount": "100000"}],
+    "rewards": [ { "q" : 0, "amount": "200000"}, { "q" : 1, "amount": "200000"}, { "q" : 2, "amount": "200000"}, { "q" : 3, "amount": "200000"}],
     "bridge": {
         "maxLockLimit" : "40000",
         "maxReleaseLimit": "80000",
@@ -18,8 +18,5 @@ module.exports = {
             "addresses": ["0x869fAD6004c59fd426939855062aF6f1378817a4"],
             "active": [true]
         }
-    },
-    "dailyRewards": {
-        "amount": "60000"
     }
 }
